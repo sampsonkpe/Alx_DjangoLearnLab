b9cdbc79cad89f1230278540d7cfbc5b0a8d7c4f
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-07-17 16:53:09 +0000

    Completed Task 1: Added function-based and class-based views with templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,8 @@
-kfrom django.urls import path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.book_list, name='book_list'),
+    path('books/', views.book_list, name='book_list'),  # Function-based view
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,28 +1,19 @@
-from django.shortcuts import render
-from .models import Book
-
-def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
-
-from django.views.generic.detail import DetailView
-from .models import Library
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
-
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
+from django.shortcuts import render, get_object_or_404
+from django.views import View
 from .models import Book, Library
 
-def list_books(request):
+# Function-based view to list books
+def book_list(request):
     books = Book.objects.select_related('author').all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, 'relationship_app/book_list.html', {'books': books})
 
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+# Class-based view to display library details
+class LibraryDetailView(View):
+    def get(self, request, pk):
+        library = get_object_or_404(Library, pk=pk)
+        books = Book.objects.filter(library=library).select_related('author')
+        return render(request, 'relationship_app/library_detail.html', {
+            'library': library,
+            'books': books
+        })
 
