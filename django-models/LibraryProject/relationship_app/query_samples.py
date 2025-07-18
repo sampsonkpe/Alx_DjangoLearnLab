e09e4546be2e32f171d0f50a6ac352fbb371b9d0
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-07-18 10:40:16 +0000

    Fix: Add missing query for author books and librarian retrieval

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,22 @@
-from .models import Library, Book, Author
+from .models import Book, Library
 
 # 1. List all books in a specific library
 def list_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()
+    except Library.DoesNotExist:
+        return []
 
 # 2. Query all books by a specific author
 def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return author.books.all()
+    return Book.objects.filter(author=author_name)
 
 # 3. Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.librarian
+    except Library.DoesNotExist:
+        return None
+
