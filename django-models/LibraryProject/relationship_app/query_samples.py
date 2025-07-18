4f3a5a5d7ed69c2f989aa55c85586ba763a2a351
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-07-18 10:48:07 +0000

    Fix: Add exact queries for author books and librarian retrieval

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,4 +1,4 @@
-from relationship_app.models import Library, Author
+from relationship_app.models import Library, Author, Book
 
 # --- List all books in a library ---
 library_name = "Central Library"
@@ -9,12 +9,11 @@ print(books)
 # --- Query all books by a specific author ---
 author_name = "Some Author"
 author = Author.objects.get(name=author_name)
-books = author.books.all()
-print(books)
+books_by_author = Book.objects.filter(author=author)
+print(books_by_author)
 
 # --- Retrieve the librarian for a library ---
-library_name = "Central Library"
-library = Library.objects.get(name=library_name)
+library = Library.objects.get(name="Central Library")
 librarian = library.librarian
 print(librarian)
 
