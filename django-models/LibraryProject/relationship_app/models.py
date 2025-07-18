c687794cb3293fb5e8c4e51617e2facceb9e7af1
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-07-18 11:57:52 +0000

    Add __str__ methods to all models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,19 +6,20 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
 class Library(models.Model):
     name = models.CharField(max_length=150)
-    books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, related_name='books', on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
