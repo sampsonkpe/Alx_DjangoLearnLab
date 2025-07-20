822a226c859acda20cf81b3c8d55892098941ea2
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-07-20 23:36:00 +0000

    Used LoginView and LogoutView with template_name for auth URLs

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,19 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, register, admin_view, librarian_view, member_view
+from .views import list_books, LibraryDetailView, register
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
-    # EXPLICITLY write these lines without wrapping or aliasing!
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 
     path('register/', register, name='register'),
-
-    path('admin-role/', admin_view, name='admin_view'),
-    path('librarian-role/', librarian_view, name='librarian_view'),
-    path('member-role/', member_view, name='member_view'),
 ]
 
