cb8f3d9ad20babe47ebf2217a4a040c76ab5adbf
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-07-20 23:16:41 +0000

    Completed Task 3: Implemented user authentication views and template structure

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView, user_login, user_logout, register
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
     path('register/', register, name='register'),
 ]
 
