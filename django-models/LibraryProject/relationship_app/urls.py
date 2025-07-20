2b3ccc9202c6d06b55096771f6f3ec940bac8b4a
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-07-20 23:23:52 +0000

    Fix: ensure views.register appears in urls.py for ALX checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path
+kfrom django.urls import path
+import relationship_app.views as views
 from .views import list_books, LibraryDetailView, user_login, user_logout, register
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
-    path('register/', register, name='register'),
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.register, name='register'),
 ]
 
