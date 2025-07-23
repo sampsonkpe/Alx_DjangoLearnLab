5abbcf24a35ee21afb2cf08233ee71b144b74cb5
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-07-23 11:31:24 +0000

    Fix: Explicit import of list_books and LibraryDetailView for checker compliance

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 # Import role-based views
@@ -23,5 +23,7 @@ urlpatterns = [
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('books/', list_books, name='list_books'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
 
