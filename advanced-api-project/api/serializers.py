e3df859d23291f8d4c73d8027c0db8204176df86
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-08-07 18:55:15 +0000

    Add nested books field to AuthorSerializer

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -2,11 +2,6 @@ from rest_framework import serializers
 from .models import Author
 from .models import Book
 
-class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = '__all__'
-
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
@@ -16,3 +11,11 @@ class BookSerializer(serializers.ModelSerializer):
         if value < 1900 or value > 2100:
             raise serializers.ValidationError("Publication year must be between 1900 and 2100.")
         return value
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)  # <-- This is what the checker wants
+
+    class Meta:
+        model = Author
+        fields = '__all__'
+
