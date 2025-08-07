1d99bef4263ce4d09a053b5c2469b38af0d3aa9d
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-08-07 18:51:45 +0000

    Add publication_year field and serializers

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -10,7 +10,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    published_year = models.DateField()
+    publication_year = models.DateField()
     isbn = models.CharField(max_length=13, default="0000000000000")
 
     def __str__(self):
