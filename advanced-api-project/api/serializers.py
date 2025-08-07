be1e7331084dc9494ff4cea6c79e55646094ff10
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-08-07 18:53:10 +0000

    Add custom validation to BookSerializer

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -12,3 +12,7 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = '__all__'
 
+    def validate_publication_year(self, value):
+        if value < 1900 or value > 2100:
+            raise serializers.ValidationError("Publication year must be between 1900 and 2100.")
+        return value
