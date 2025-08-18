ba99cbc31080f2ee14973b262d3248ef72f90814
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-08-18 13:00:36 +0000

    Fixed serializers.py by including from rest_framework.authtoken.models import Token

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,17 +2,15 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
-
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
