cf88f19e3df6e7077c832d1006c3fbca370cecd3
Sampson Kojo Kpe <sampsonkpe2@icloud.com>
2025-08-18 12:58:54 +0000

    Fixed serializers.py by including from rest_framework.authtoken.models import Token

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,15 +1,15 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class UserRegistrationSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture')
+        fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -19,11 +19,6 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
+        Token.objects.create(user=user)
         return user
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
 
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,23 +1,18 @@
-from rest_framework import generics, permissions
-from django.contrib.auth import get_user_model
-from .serializers import UserRegistrationSerializer, UserSerializer
+from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer
 
 User = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserRegistrationSerializer
-    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
 
-
-class ProfileView(generics.RetrieveUpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_object(self):
-        return self.request.user
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = User.objects.get(username=response.data['username'])
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'user': response.data, 'token': token.key})
 
